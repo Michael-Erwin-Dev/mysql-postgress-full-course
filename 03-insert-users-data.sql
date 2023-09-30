df55ae3f9112fc684275656336893b112071f8a4
Michael Erwin <michaelerwinconsulting@gmail.com>
2023-09-30 04:29:14 +0200

    43. Inserting & Querying More Data

--- 03-insert-users-data.sql
+++ 03-insert-users-data.sql
@@ -1,10 +1,30 @@
+-- INSERT INTO users (
+--         full_name,
+--         yearly_salary,
+--         current_status
+--     )
+-- VALUES (
+--         'John Doe',
+--         50000,
+--         'self-employed'
+--     );
 INSERT INTO users (
         full_name,
         yearly_salary,
         current_status
     )
 VALUES (
-        'John Doe',
-        50000,
-        'self-employed'
+        'Joe Shmoe',
+        25000,
+        'employed'
+    );
+INSERT INTO users (
+        full_name,
+        yearly_salary,
+        current_status
+    )
+VALUES (
+        'Alessio Franceschi',
+        0,
+        'unemployed'
     );
