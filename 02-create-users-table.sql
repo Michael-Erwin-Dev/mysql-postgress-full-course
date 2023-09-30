3a8be58545c6fdf9bcd41793ca0f0bd0c51db236
Michael Erwin <michaelerwinconsulting@gmail.com>
2023-09-30 04:11:28 +0200

    41. Working with Enums

--- 02-create-users-table.sql
+++ 02-create-users-table.sql
@@ -1,4 +1,8 @@
+-- PostgreSql enum type => we need to create a custom data type 
+CREATE TYPE employment_status AS ENUM ('employed', 'self-employed', 'unemployed');
 CREATE TABLE users (
     full_name VARCHAR(200),
     yearly_salary INT,
+    --current_status ENUM('employed', 'self-employed', 'unemployed') -- mysql sintax for enums
+    current_status employment_status
 );
