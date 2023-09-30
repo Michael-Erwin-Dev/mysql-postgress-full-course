354e06925d11fee434cd81f17094c70b06a9a021
Michael Erwin <michaelerwinconsulting@gmail.com>
2023-09-30 16:20:02 +0200

    51. Updating Tables & Columns

---
+++ 11-update-tables.sql
@@ -0,0 +1,12 @@
+-- ALTER TABLE employers
+-- ALTER COLUMN yearly_revenue
+-- SET DATA TYPE FLOAT;
+-- POSTGRES ONLY
+-- ALTER TABLE employers
+-- MODIFY COLUMN yearly_revenue FLOAT(5, 2);
+-- MYSQL ONLY
+-- ALTER TABLE users
+-- ALTER COLUMN full_name
+-- SET DATA TYPE VARCHAR(300); --POSTGRESS ONLY
+ALTER TABLE users
+MODIFY COLUMN full_name VARCHAR(300);
