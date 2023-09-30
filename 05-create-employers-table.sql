b06cb9acdb3728d78ab16b7d4c66f7a3b248e461
Michael Erwin <michaelerwinconsulting@gmail.com>
2023-09-30 15:24:15 +0200

    45. Adding Boolean Value Types

--- 05-create-employers-table.sql
+++ 05-create-employers-table.sql
@@ -1,7 +1,6 @@
 CREATE TABLE employers (
     company_name VARCHAR(255),
     company_address VARCHAR(255),
-    --yearly_revenue FLOAT(5,2), -- 5 digits, 2 of which are decimals aproximstion
     yearly_revenue NUMERIC(5, 2),
-    -- 5 digits, 2 of which are decimals exact value
+    is_hiring BOOLEAN
 );
